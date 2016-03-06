3fa1262acb6d7a3720d924cc9da07209c4968f9f
Hugo <hugo.hornquist@gmail.com>
2016-03-06 23:20:26 +0100

    added basic nettwork packet code

---
+++ src/other/Packet.java
@@ -0,0 +1,20 @@
+package other;
+
+import java.io.Serializable;
+
+public class Packet implements Serializable {
+	private static final long serialVersionUID = -6711932079263224190L;
+
+	private String message;
+
+	public Packet( String message ) {
+		this.message = message;
+	}
+
+	/**
+	 * @return the message
+	 */
+	public String getMessage() {
+		return message;
+	}
+}
