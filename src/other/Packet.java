930977ee2a862b9d072340d92a1722421ed52e90
Hugo <hugo.hornquist@gmail.com>
2016-03-07 00:44:18 +0100

    added author to packets

--- src/other/Packet.java
+++ src/other/Packet.java
@@ -5,10 +5,19 @@ import java.io.Serializable;
 public class Packet implements Serializable {
 	private static final long serialVersionUID = -6711932079263224190L;
 
+	private String author;
 	private String message;
 
-	public Packet( String message ) {
+	public Packet( String author, String message ) {
+		this.author = author;
 		this.message = message;
+	}
+
+	/**
+	 * @return the author
+	 */
+	public String getAuthor() {
+		return author;
 	}
 
 	/**
