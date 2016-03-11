35432a8899bcab6f786b7e2ae4dee05c1d9d3758
Hugo <hugo.hornquist@gmail.com>
2016-03-11 09:56:33 +0100

    server now accepts any number of clients. And it doesn't accept command line input

--- src/server/Server.java
+++ src/server/Server.java
@@ -2,41 +2,84 @@ package server;
 
 import other.Packet;
 import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Server {
-	private ServerSocket server;
-	private Socket client;
 
-	private ObjectOutputStream os;
+	private ServerSocket server;
+	private ArrayList<ObjectOutputStream> outStreams;
 
 	public Server() throws IOException {
+		outStreams = new ArrayList<ObjectOutputStream>();
 		server = new ServerSocket(8080);
 
-		client = server.accept();
-		os = new ObjectOutputStream(client.getOutputStream());
+		new Thread( new ConnectionThread() ).start();
 
-		System.out.println( "client ready!" );
+	}
 
-		Scanner sc = new Scanner( System.in );
-
-		System.out.println( "send a message to the client" );
-		String sendStr = "";
-		while( !sendStr.equals("STOP") ) {
-			System.out.print(":");
-			sendStr = sc.next();
-
-			try {
-				os.writeObject( new Packet(sendStr) );
-			} catch( IOException e ) {
-				e.printStackTrace();
+	private class ConnectionThread implements Runnable {
+		@Override
+		public void run() {
+			while( true ) {
+				try {
+					new ClientThread( server.accept() );
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
 			}
 		}
+	}
+	
+	private class ClientThread implements Runnable {
 
-		sc.close();
-		os.close();
+		private Socket client;
+		private ObjectInputStream in;
+
+		public ClientThread( Socket client ) {
+			this.client = client;
+
+			try {
+				outStreams.add(new ObjectOutputStream(client.getOutputStream()));
+				in = new ObjectInputStream( client.getInputStream() );
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+
+			new Thread( this ).start();
+		}
+
+		private void cleanup() {
+			try {
+				in.close();
+				// TODO note that this might not work.
+				// Possibly replace the arrayList with a hashmap and use the client object as the key
+				outStreams.remove( client.getOutputStream() );
+				client.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+
+		}
+
+		@Override
+		public void run() {
+			while( true ) {
+				try {
+					Packet inPacket = (Packet) in.readObject();
+					for( ObjectOutputStream out : outStreams ) {
+						System.out.println( inPacket );
+						out.writeObject( inPacket );
+					}
+				} catch( Exception e ) {
+					// TODO possibly better exception handling
+					e.printStackTrace();
+					this.cleanup();
+				}
+			}
+		}
 	}
 }
